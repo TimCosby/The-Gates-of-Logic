ff2b3a536f2de22658bf888799a16b0341924001
Tiger <tigeryu7@gmail.com>
2018-06-01 00:32:03 -0400

    Updated ConditionSensitiveTrigger.cs and Trigger.cs to support the ability to choose gate behavior. (Need to update level 1)

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -7,4 +7,7 @@ public class Trigger : MonoBehaviour {
 	public bool Triggered = false;
 	public bool Inversed = false;
 	public bool SingleUse = false;
+	public bool AND = false;
+	public bool OR = false;
+	public bool XOR = false;
 }
