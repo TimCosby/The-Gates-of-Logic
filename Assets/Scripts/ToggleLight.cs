0082e6f1ec486f5681365655d1211fa9933b7cfb
Tim Cosby <tim470773@gmail.com>
2018-06-02 01:25:18 -0400

    Added more to the redesign of level 1.

---
+++ Assets/Scripts/ToggleLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLight : MonoBehaviour {
+
+	public bool Toggled = true;
+	public Trigger Trigger;
+
+	private void Start() {
+		if (Trigger == null) {
+			Trigger = GetComponent<Trigger>();
+		}
+	}
+
+	private void Update() {
+		Toggled = Trigger.Triggered;
+		GetComponent<Light>().enabled = Toggled;
+	}
+}
