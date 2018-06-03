c29e869ea0e46dc6e4d7bd8bc64d46d0e40c4acc
Tim Cosby <tim470773@gmail.com>
2018-06-03 18:41:54 -0400

    Finished level 1

---
+++ Assets/Scripts/DisableTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableTrigger : Trigger {
+
+	public Trigger Trigger;
+	public Trigger[] TriggersToDisable;
+
+	private void Start() {
+		if (Trigger == null) {
+			Trigger = GetComponent<Trigger>();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Trigger.Triggered) {
+			for (int i = 0; i < TriggersToDisable.Length; i++) {
+				if (Inversed)
+					TriggersToDisable[i].enabled = true;
+				else
+					TriggersToDisable[i].enabled = false;
+			}
+		}
+		else {
+			for (int i = 0; i < TriggersToDisable.Length; i++) {
+				if (Inversed)
+					TriggersToDisable[i].enabled = false;
+				else
+					TriggersToDisable[i].enabled = true;
+			}
+		}
+	}
+}
