b7265cfaa44b1fe19fbdd9348d3dc7f8e742ae84
Tim Cosby <tim470773@gmail.com>
2018-06-03 16:52:52 -0400

    Fixed audio bugs at startup.

--- Assets/Scripts/MovePosition.cs
+++ Assets/Scripts/MovePosition.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MovePosition : MonoBehaviour {
 
 	public Trigger Trigger;
-	public Vector3 ToPos;
+	public Vector3 OffSet;
 	private Vector3 FromPos;
 	private Transform obj;
 	public float speed = 2f;
+	public bool X = true;
+	public bool Y = true;
+	public bool Z = true;
 
 	private void Start() {
 		if (Trigger == null) {
@@ -20,11 +23,33 @@ public class MovePosition : MonoBehaviour {
 
 	// Update is called once per frame
 	private void Update () {
+		Vector3 ObjectPos = GetComponent<Transform>().position;
+		Vector3 ToPos = ObjectPos;
+
 		if (Trigger.Triggered) {
-			obj.position = Vector3.MoveTowards(obj.position, FromPos + ToPos, speed * Time.deltaTime); //obj.Translate(CalculateNewPos() * Time.deltaTime);
+			if (X) {
+				ToPos.x = FromPos.x + OffSet.x;
+			}
+			if (Y) {
+				ToPos.y = FromPos.y + OffSet.y;
+			}
+			if (Z) {
+				ToPos.z = FromPos.z + OffSet.z;
+			}
+
+			obj.position = Vector3.Lerp(ObjectPos, ToPos, speed * Time.deltaTime);// MoveTowards(obj.position, FromPos + ToPos, speed * Time.deltaTime); //obj.Translate(CalculateNewPos() * Time.deltaTime);
 		}
 		else {
-			obj.position = Vector3.MoveTowards(obj.position, FromPos, speed * Time.deltaTime); //obj.Translate(CalculatePos() * Time.deltaTime);
+			if (X) {
+				ToPos.x = FromPos.x;
+			}
+			if (Y) {
+				ToPos.y = FromPos.y;
+			}
+			if (Z) {
+				ToPos.z = FromPos.z;
+			}
+			obj.position = Vector3.Lerp(ObjectPos, ToPos, speed * Time.deltaTime);//Vector3.MoveTowards(obj.position, FromPos, speed * Time.deltaTime); //obj.Translate(CalculatePos() * Time.deltaTime);
 		} 
 	}
 }
---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+	public Trigger Trigger;
+	private AudioSource Audio;
+	private bool Played = false;
+	public bool PlayOnToggle = false;
+	private bool StartMakingSound = false;
+	private bool DefaultState;
+
+	private void Start() {
+		if (Trigger == null) {
+			Trigger = GetComponent<Trigger>();
+		}
+		DefaultState = Trigger.Triggered;
+
+		Audio = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (!StartMakingSound && DefaultState != Trigger.Triggered) {
+			StartMakingSound = true;
+		}
+
+		if (StartMakingSound) {
+			if (Trigger.Triggered && !Played) {
+				if (Audio.isPlaying) {
+					Audio.Stop();
+				}
+				Audio.Play();
+				Played = true;
+			}
+			else if (!Trigger.Triggered && Played) {
+				if (Audio.isPlaying) {
+					Audio.Stop();
+				}
+				Played = false;
+				if (PlayOnToggle) {
+					Audio.Play();
+				}
+			}
+		}
+	}
+}
