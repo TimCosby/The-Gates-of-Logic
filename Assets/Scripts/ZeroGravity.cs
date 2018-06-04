49ec40634fe4703faf6e2b6e1e413208cc08e020
Tiger <tigeryu7@gmail.com>
2018-06-04 14:49:42 -0400

    - Implemented the a UI object that shows the status of "Charge" for ZeroGravity. ZeroGravity now depends on the charge instead of time and delay.

--- Assets/Scripts/ZeroGravity.cs
+++ Assets/Scripts/ZeroGravity.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ZeroGravity : MonoBehaviour {
 
-	public float TimeLimit = 0;
-	public float Delay = 0;
+	public Image ZeroGravityBar;
+	public float ZeroGravityCharge = 0;
+	public float ZeroGravityMax = 100;
+	//public float TimeLimit = 0;
+	//public float Delay = 0;
 	public string[] AffectedObjects;
-	private float TimeStart = -10;
-	private float TimeEnd = -10;
+	//private float TimeStart = -10;
+	//private float TimeEnd = -10;
 	private bool Toggled = false;
 	private bool Unpressed = true;
 	public AudioClip BackgroundMusic;
@@ -16,15 +20,17 @@ public class ZeroGravity : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		float ratio = ZeroGravityCharge / ZeroGravityMax;
 		bool GravKey = Input.GetKey(KeyCode.E);
+ 		ZeroGravityBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
 
-		if (Toggled && (Input.GetKeyUp(KeyCode.E) || Time.time - TimeStart - TimeLimit > 0)) {
+		if (Toggled && (Input.GetKeyUp(KeyCode.E) || ZeroGravityCharge == 0)) {
 			Debug.Log("Off");
 			GetComponent<CharacterMovement>().ZeroG = false;
 			
 			GetComponent<AudioSource>().clip = BackgroundMusic;
 			GetComponent<AudioSource>().Play();
-			TimeEnd = Time.time;
+			//TimeEnd = Time.time;
 			Toggled = false;
 
 			for (int i = 0; i < AffectedObjects.Length; i++) {
@@ -34,23 +40,30 @@ public class ZeroGravity : MonoBehaviour {
 				}
 			}
 		}
-		else if (Unpressed && !Toggled && GravKey && Time.time - TimeEnd - Delay > 0) {
+		else if (Unpressed && !Toggled && GravKey && ZeroGravityCharge == 100) {
 			Debug.Log("On");
 			GetComponent<CharacterMovement>().ZeroG = true;
 
 			GetComponent<AudioSource>().clip = ZeroGMusic;
 			GetComponent<AudioSource>().Play();
-			TimeStart = Time.time;
+			//TimeStart = Time.time;
 			Toggled = true;
 		}
 
 		if (Toggled) {
+			if (ZeroGravityCharge != 0) {
+				ZeroGravityCharge -= 1;
+			}
 			for (int i = 0; i < AffectedObjects.Length; i++) {
 				GameObject[] Objects = GameObject.FindGameObjectsWithTag(AffectedObjects[i]);
 				for (int j = 0; j < Objects.Length; j++) {
 					Objects[j].GetComponent<Rigidbody>().useGravity = false;
 				}
 			}
+		} else {
+			if (ZeroGravityCharge != ZeroGravityMax ) {
+				ZeroGravityCharge += 1;
+			}		
 		}
 	}
 }
