f76f49835aa85ecc5a0cb478840124e1b94dc165
Tim Cosby <tim470773@gmail.com>
2018-06-13 20:09:11 -0400

    Made switch to next level script

---
+++ Assets/Scripts/GoToScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour {
+
+	public Trigger Trigger;
+	public string SceneName;
+
+	// Use this for initialization
+	void Start () {
+		if (Trigger == null) {
+			Trigger = GetComponent<Trigger>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Trigger.Triggered) {
+			SceneManager.LoadScene(SceneName);
+		}
+	}
+}
