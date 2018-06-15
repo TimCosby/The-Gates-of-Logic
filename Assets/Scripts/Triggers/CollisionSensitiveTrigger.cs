737b0d09f13f4ebc7084e8b6c5edec37a5ed7c92
Tim Cosby <tim470773@gmail.com>
2018-06-15 11:57:49 -0400

    Fixed buttons.

---
+++ Assets/Scripts/Triggers/CollisionSensitiveTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSensitiveTrigger : Trigger {
+
+	public string[] tags;
+	public bool Toggleable = false;
+	public float UnpressDelay = 0f;
+	private float LastPress = -50f;
+
+	private void OnCollisionEnter(Collision collision) {
+		if (Toggleable) {
+			if (LastPress <= Time.time - UnpressDelay) {
+				Triggered = !Triggered;
+			}
+		}
+		else {
+			for (int i = 0; i < tags.Length; i++) {
+				if (tags[i] == collision.gameObject.tag) {
+					if (Inversed) {
+						Triggered = false;
+					}
+					else {
+						Triggered = true;
+					}
+				}
+			}
+		}
+	}
+
+	private void OnCollisionExit(Collision collision) {
+		if (!Toggleable && LastPress <= Time.time - UnpressDelay) {
+			if (!SingleUse) {
+				if (Inversed) {
+					Triggered = true;
+				}
+				else {
+					Triggered = false;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Triggers/TouchSensitiveTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchSensitiveTrigger : Trigger {
+
+	public string TriggerTag;
+	public bool Toggleable = false;
+	public float Delay = 0f;
+	private float LastPress = -100f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == TriggerTag && LastPress <= Time.time - Delay) {
+			if (Toggleable) {
+				Triggered = !Triggered;
+			}
+			else if (Inversed) {
+				Triggered = false;
+			}
+			else {
+				Triggered = true;
+			}
+			LastPress = Time.time;
+		}
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+		if (!Toggleable && !SingleUse && LastPress <= Time.time - Delay) {
+			if (other.tag == TriggerTag) {
+				if (Inversed)
+					Triggered = true;
+				else
+					Triggered = false;
+				LastPress = Time.time;
+			}
+		}
+    }
+}
