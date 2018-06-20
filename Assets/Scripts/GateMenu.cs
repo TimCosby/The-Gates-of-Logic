8fa1482bb1d427a8923a4f44859bba57736c5bb1
Tim Cosby <tim470773@gmail.com>
2018-06-20 01:28:44 -0400

    Fixed menu and finished level 2

--- Assets/Scripts/GateMenu.cs
+++ Assets/Scripts/GateMenu.cs
@@ -2,59 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Collections.Specialized;
 
 public class GateMenu : MonoBehaviour {
 
-	private short CurrentGate = 0;
-	private short EnableSize = 0;
-	private Hashtable Quantity;
-	private Hashtable Gates;
-	private Transform QuantityObject;
 	public bool AND = true;
 	public bool OR = true;
+	public Trigger Trigger;
+	private int CurrentGate = 1;
+
+	private Dictionary<string, int> Quantity;
+	private Dictionary<string, GameObject> Gates;
+	private Transform QuantityObject;
 
 	// Use this for initialization
 	void Start () {
-		Quantity = new Hashtable();
-		Gates = new Hashtable();
+		Quantity = new Dictionary<string, int>();
+		Gates = new Dictionary<string, GameObject>();
+		Trigger = GetComponent<Trigger>();
 		QuantityObject = transform.GetChild(0);
 
-		Gates.Add(EnableSize, transform.GetChild(1).gameObject);
+		Gates.Add("EMPTY", transform.GetChild(1).gameObject);
 
-		if (AND) {
-			EnableSize++;
-			Quantity.Add(EnableSize, 0);
-			Gates.Add(EnableSize, transform.GetChild(2).gameObject);
-		}
-		if (OR) {
-			EnableSize++;
-			Quantity.Add(EnableSize, 1);
-			Gates.Add(EnableSize, transform.GetChild(3).gameObject);
-		}
+		Quantity.Add("AND", 0);
+		Gates.Add("AND", transform.GetChild(2).gameObject);
+		Quantity.Add("OR", 1);
+		Gates.Add("OR", transform.GetChild(3).gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (enabled) {
-			if (Input.GetMouseButtonDown(0)) {
-				((GameObject)Gates[CurrentGate]).SetActive(false);
+		if (Input.GetMouseButtonDown(0)) {
+			if (CurrentGate == 0) { // Switching from OR to None
+				QuantityObject.gameObject.SetActive(false);
 
-				if (CurrentGate == 0) {
-					QuantityObject.gameObject.SetActive(true);
-				}
+				Gates["OR"].SetActive(false);
+				Gates["EMPTY"].SetActive(true);
+			}
+			else if (CurrentGate == 1) { // Switching from None to AND 
+				QuantityObject.gameObject.SetActive(true);
 
-				CurrentGate++;
+				Gates["EMPTY"].SetActive(false);
+				Gates["AND"].SetActive(true);
 
-				if (CurrentGate > EnableSize) {
-					QuantityObject.gameObject.SetActive(false);
-					CurrentGate = 0;
+				int tempQuantity = Quantity["AND"];
+				if (tempQuantity == 0) {
+					Trigger.Triggered = false;
 				}
 				else {
-					QuantityObject.gameObject.GetComponent<TextMeshPro>().text = ((int)(Quantity[CurrentGate])).ToString();
+					Trigger.Triggered = true;
 				}
 
-				((GameObject)Gates[CurrentGate]).SetActive(true);
+				QuantityObject.GetComponent<TextMeshPro>().SetText(tempQuantity.ToString());
 			}
+			else if (CurrentGate == 2) {
+				QuantityObject.gameObject.SetActive(true);
+
+				Gates["AND"].SetActive(false);
+				Gates["OR"].SetActive(true);
+
+				int tempQuantity = Quantity["OR"];
+				if (tempQuantity == 0) {
+					Trigger.Triggered = false;
+				}
+				else {
+					Trigger.Triggered = true;
+				}
+
+				QuantityObject.GetComponent<TextMeshPro>().SetText(tempQuantity.ToString());
+				CurrentGate = -1;
+			}
+
+			CurrentGate++;
 		}
+	}
+
+	public void AddItem(string gate, int quantity) {
+		Quantity[gate] += quantity;
 	}
 }
