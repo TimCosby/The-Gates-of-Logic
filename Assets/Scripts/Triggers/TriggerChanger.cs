a7a13d25d7bdc3325e702546ef40e0115c2e435a
Tiger <tigeryu7@gmail.com>
2018-06-28 23:36:56 -0400

    completed the basic layout of level 5. gates and logic do not work yet. tooltips also have not yet been updated

---
+++ Assets/Scripts/Triggers/TriggerChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChanger : MonoBehaviour {
+
+    public Trigger Trigger;
+    public float delay;
+    private float lastChange;
+
+	// Use this for initialization
+	void Start () {
+		if (Trigger == null)
+        {
+            Trigger = GetComponent<Trigger>();
+        }
+        lastChange = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - lastChange > delay)
+        {
+            Trigger.Triggered = !Trigger.Triggered;
+            lastChange = Time.time;
+        }
+	}
+}
