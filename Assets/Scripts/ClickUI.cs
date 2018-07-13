cc086d24ec8c00f861857ffd7769b029e608be29
Tim Cosby <tim470773@gmail.com>
2018-07-13 11:56:23 -0400

    Started dark mode on level 2 & click interface for gates.

---
+++ Assets/Scripts/ClickUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (EventSystem.current.IsPointerOverGameObject()) {
+			Debug.Log("nu");
+			if (Input.GetMouseButtonDown(0)) {
+				Debug.Log("ecchi");
+			}
+		}
+	}
+}
