a5b9d72ca3bfbe2962f97fd3a2c27bacbc941847
Tiger <tigeryu7@gmail.com>
2018-07-17 14:50:06 -0400

    Added: Polishes to level 2 (now has power button and outlines for wires), fixes for problems with disablecomponent and singleuse, a new script SlaveConditionSensitiveTrigger

--- Assets/Scripts/DisableComponent.cs
+++ Assets/Scripts/DisableComponent.cs
@@ -6,30 +6,40 @@ public class DisableComponent : Trigger {
 
 	public Trigger Trigger;
 	public MonoBehaviour[] ComponentsToDisable;
+    private bool hasUsed;
 
 	private void Start() {
 		if (Trigger == null) {
 			Trigger = GetComponent<Trigger>();
 		}
+        hasUsed = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Trigger.Triggered) {
-			for (int i = 0; i < ComponentsToDisable.Length; i++) {
-				if (Inversed)
-					ComponentsToDisable[i].enabled = true;
-				else
-					ComponentsToDisable[i].enabled = false;
-			}
-		}
-		else {
-			for (int i = 0; i < ComponentsToDisable.Length; i++) {
-				if (Inversed)
-					ComponentsToDisable[i].enabled = false;
-				else
-					ComponentsToDisable[i].enabled = true;
-			}
-		}
+        if (!SingleUse || (SingleUse && !hasUsed))
+        {
+            if (Trigger.Triggered)
+            {
+                for (int i = 0; i < ComponentsToDisable.Length; i++)
+                {
+                    if (Inversed)
+                        ComponentsToDisable[i].enabled = true;
+                    else
+                        ComponentsToDisable[i].enabled = false;
+                }
+                hasUsed = true;
+            }
+            else
+            {
+                for (int i = 0; i < ComponentsToDisable.Length; i++)
+                {
+                    if (Inversed)
+                        ComponentsToDisable[i].enabled = false;
+                    else
+                        ComponentsToDisable[i].enabled = true;
+                }
+            }
+        }
 	}
 }
---
+++ Assets/Scripts/Triggers/SlaveConditionSensitiveTrigger.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlaveConditionSensitiveTrigger : Trigger {
+
+    public Trigger Master;
+	public Trigger[] TriggerObjects;
+	private bool StartingTrig;
+	private bool IsAND;
+	private bool IsOR;
+	private bool IsXOR;
+
+	public void Start() {
+		StartingTrig = Triggered;
+		IsAND = AND;
+		IsOR = OR;
+		IsXOR = XOR;
+	}
+
+	private void Update() {
+        if (Master.Triggered)
+        {
+            if (!SingleUse || (SingleUse && StartingTrig == Triggered))
+            {
+                int NumTrues = 0;
+                for (int i = 0; i < TriggerObjects.Length; i++)
+                {
+                    if (TriggerObjects[i].gameObject.activeSelf && TriggerObjects[i].Triggered == true)
+                    {
+                        NumTrues++;
+                    }
+                }
+
+                if (NumTrues < 1)
+                {
+                    Triggered = false;
+                }
+                else
+                {
+                    if (IsOR)
+                    {
+                        Triggered = true;
+                    }
+                    else if (IsXOR && NumTrues == 1)
+                    {
+                        Triggered = true;
+                    }
+                    else if (IsXOR)
+                    {
+                        Triggered = false;
+                    }
+                    else if (IsAND && NumTrues == TriggerObjects.Length)
+                    {
+                        Triggered = true;
+                    }
+                    else if (IsAND)
+                    {
+                        Triggered = false;
+                    }
+                    else
+                    {
+                        Triggered = true;
+                    }
+                }
+
+                if (Inversed)
+                {
+                    Triggered = !Triggered;
+                }
+            }
+        }
+        else
+        {
+            Triggered = false;
+        }
+	}
+}
