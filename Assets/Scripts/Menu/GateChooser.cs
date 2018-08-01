9ba242b0241560dcfbe0a20fb95e960a69fb1b29
Tim Cosby <tim470773@gmail.com>
2018-07-31 20:55:35 -0400

    Implemented new menu.

---
+++ Assets/Scripts/Menu/GateChooser.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateChooser : Trigger {
+
+	private Dictionary<int, string> GateName;
+	private Dictionary<int, GameObject> GateObject;
+
+	public Trigger TouchTrigger;
+
+	public int StartingGate = 0;
+	private int CurrentGate;
+
+	public GameObject canvas;
+	private UIMenu menu;
+
+	private bool WasTriggered = false;
+
+	// Use this for initialization
+	void Start () {
+		//canvas = GameObject.FindGameObjectWithTag("UIMenu");
+		GateName = new Dictionary<int, string>();
+		GateObject = new Dictionary<int, GameObject>();
+		menu = canvas.GetComponent<UIMenu>();
+
+		CurrentGate = StartingGate + 1;
+
+		for (int i = 1; i < transform.childCount; i++) {
+			Debug.Log(i + " " + transform.GetChild(i).name);
+			GateName.Add(i, transform.GetChild(i).name);
+			GateObject.Add(i, transform.GetChild(i).gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (TouchTrigger.Triggered) {
+			WasTriggered = true;
+			GateObject[CurrentGate].SetActive(true);
+			menu.EnableUI(GateName[CurrentGate]);
+
+			if (Input.GetKeyDown("space")) {
+				GateObject[CurrentGate].SetActive(false);
+				Debug.Log("Gate: " + GateObject[CurrentGate] + " " + CurrentGate);
+				Debug.Log("Response: " + menu.IsGateActive(GateObject[CurrentGate].GetComponent<Renderer>().sharedMaterial));
+				if (menu.IsGateActive(GateObject[CurrentGate].GetComponent<Renderer>().sharedMaterial)) {
+					menu.ModifyGate(GateName[CurrentGate], 1);
+				}
+
+				CurrentGate++;
+				if (CurrentGate == transform.childCount) {
+					CurrentGate = 1;
+				}
+
+				GateObject[CurrentGate].SetActive(true);
+				menu.SetFocusGate(GateName[CurrentGate]);
+
+				GateObject[CurrentGate].GetComponent<Renderer>().material = menu.GetGateMaterial(GateName[CurrentGate]);
+				if (menu.IsGateActive(GateObject[CurrentGate].GetComponent<Renderer>().sharedMaterial)) {
+					menu.ModifyGate(GateName[CurrentGate], -1);
+					Triggered = true;
+				}
+				else {
+					Triggered = false;
+				}
+			}
+
+		}
+		else if (WasTriggered) {
+			if (!menu.IsGateActive(GateObject[CurrentGate].GetComponent<Renderer>().sharedMaterial)) {
+				GateObject[CurrentGate].SetActive(false);
+			}
+			menu.DisableUI();
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/UIMenu.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenu : MonoBehaviour {
+
+	private Dictionary<string, GameObject> GateObject;
+	private Dictionary<string, int> GateQuantity;
+	private Dictionary<string, int> GateSelect;
+
+	public bool AND = true;
+	public bool OR = true;
+	public bool XOR = true;
+
+	public int[] DefaultValues;
+
+	public Material OutOfStock;
+	public Material InStock;
+
+	public GameObject PlayerObject;
+
+	private string CurrentGate = "AND Gate";
+
+	private GameObject Selector;
+	private Transform Gates;
+
+	// Use this for initialization
+	void Start () {
+		GateObject = new Dictionary<string, GameObject>();
+		GateQuantity = new Dictionary<string, int>();
+		GateSelect = new Dictionary<string, int>();
+		Selector = transform.GetChild(2).gameObject;
+		Gates = transform.GetChild(3);
+
+		GateSelect.Add(Gates.GetChild(0).name, 0);
+		GateSelect.Add(Gates.GetChild(1).name, 100);
+		GateSelect.Add(Gates.GetChild(2).name, -100);
+
+		for (int i = 0; i < Gates.childCount; i++) {
+			Transform GateChild = Gates.GetChild(i);
+			GateObject.Add(GateChild.name, GateChild.gameObject);
+			if (DefaultValues.Length > i) {
+				GateQuantity.Add(GateChild.name, DefaultValues[i]);
+			}
+			else {
+				GateQuantity.Add(GateChild.name, 0);
+			}
+
+			if (GateQuantity[GateChild.name] == 0) {
+				GateObject[GateChild.name].GetComponent<Image>().color = OutOfStock.color;
+			}
+			else {
+				GateObject[GateChild.name].GetComponent<Image>().color = InStock.color;
+			}
+			UpdateText(GateChild.name);
+		}
+	}
+
+	public void EnableUI(string Gate) {
+		PlayerObject.GetComponent<CharacterMovement>().ModifyJump(false);
+		for (int i = 0; i <= Gates.childCount; i++) {
+			Debug.Log(i);
+			Debug.Log(transform.GetChild(i).name);
+			transform.GetChild(i).gameObject.SetActive(true);
+		}
+		SetFocusGate(Gate);
+	}
+
+	public void DisableUI() {
+		PlayerObject.GetComponent<CharacterMovement>().ModifyJump(true);
+		for (int i = 0; i <= Gates.childCount; i++) {
+			transform.GetChild(i).gameObject.SetActive(false);
+		}
+	}
+
+	public void ModifyGate(string Gate, int Adjustment) {
+		GateQuantity[Gate] += Adjustment;
+
+		if (GateQuantity[Gate] == 0) {
+			GateObject[Gate].GetComponent<Image>().color = OutOfStock.color;
+		}
+		else {
+			GateObject[Gate].GetComponent<Image>().color = InStock.color;
+		}
+
+		UpdateText(Gate);
+	}
+
+	public int GetGateQuantity(string Gate) {
+		return GateQuantity[Gate];
+	}
+
+	public Material GetGateMaterial(string Gate) {
+		if (GateQuantity[Gate] == 0) {
+			return OutOfStock;
+		}
+		else {
+			return InStock;
+		}
+	}
+
+	public bool IsGateActive(Material material) {
+		return material == InStock;
+	}
+
+	public string GetFocusGate() {
+		return CurrentGate;
+	}
+
+	public void SetFocusGate(string Gate) {
+		GateObject[CurrentGate].transform.GetChild(0).gameObject.SetActive(false);
+		CurrentGate = Gate;
+		GateObject[CurrentGate].transform.GetChild(0).gameObject.SetActive(true);
+		Vector3 SelectorPos = Selector.GetComponent<RectTransform>().anchoredPosition;
+
+		SelectorPos.x = GateSelect[Gate];
+
+		Selector.GetComponent<RectTransform>().anchoredPosition = SelectorPos;
+	}
+
+	private void UpdateText(string Gate) {
+		Transform GateText = GateObject[Gate].transform.GetChild(0);
+		string TempText = GateText.GetComponent<Text>().text;
+		GateText.GetComponent<Text>().text = TempText.Substring(0, TempText.Length - 1) + GateQuantity[Gate];
+
+		if (GateQuantity[Gate] != 0) {
+			GateText.GetComponent<Text>().color = new Color32(25, 142, 35, 255);
+		}
+		else {
+			GateText.GetComponent<Text>().color = new Color32(183, 28, 28, 255);
+		}
+	}
+}
