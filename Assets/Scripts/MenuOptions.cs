9a0a89d520ed0643ac4dc657377320b5cb260936
Tim Cosby <tim470773@gmail.com>
2018-08-08 17:21:53 -0400

    Created start menu.

---
+++ Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuOptions : MonoBehaviour {
+
+	public void Start() {
+		transform.GetChild(0).GetComponent<Button>().Select();
+	}
+
+	private void Update() {
+		Cursor.visible = false;
+	}
+
+	public void NewGame() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void GoToScene(string Scene) {
+		SceneManager.LoadScene(Scene);
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
